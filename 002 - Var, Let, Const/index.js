6d4783e1a910319ba46b27e37b422af67050ec0f
Manopipoca111005 <gustavoramos111005@gmail.com>
2025-02-27 20:26:35 +0000

    Aulas JS do curso frontend do vida fullstack

---
+++ 002 - Var, Let, Const/index.js
@@ -0,0 +1,31 @@
+/*var nome = "Gustavo"
+
+if (!nome) {
+  var nome = "Gustavo Ramos"
+  console.log(nome)
+}
+
+var nome = "Pipoca";
+
+console.log(nome)/*
+
+/*let nome = ""
+let sexo = "masc"
+
+if (!nome) {
+  nome = "Gustavo Ramos"
+  console.log(nome)
+  console.log(sexo)
+}
+
+nome = "Lara";
+sexo = "fem"
+
+console.log(nome)
+console.log(sexo)/*
+
+/*const NOME = "Gustavo"
+
+console.log(NOME)
+
+NOME = "Pipoca"*/
---
+++ 003 - Tipos Primitivos/index.js
@@ -0,0 +1,11 @@
+/*
+  String
+  Number
+  Boolean
+  Null
+  undefined
+*/
+
+let nome = "Gustavo Ramos";
+
+console.log(typeof nome, nome)
